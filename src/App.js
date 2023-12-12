09a357029dbd7a6658e8720a0df8e06c86c8f585
Murega14 <tedmurega@gmail.com>
2023-12-12 18:03:50 +0300

    fixed navigation links with hook error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import Navbar from './components/assets/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import ShopCategory from './pages/ShopCategory';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
+
 
 function App() {
   return (
-    <div>
+      <div className="App">
       <Navbar/>
-      
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/wine' element={<ShopCategory />} />
+        <Route path='/gin' element={<ShopCategory />} />
+        <Route path='/whiskey' element={<ShopCategory />} />
+        <Route path='/vodka' element={<ShopCategory />} />
+        <Route path='/beer' element={<ShopCategory />} />
+        <Route path='/product' element={<Product />}>
+          <Route path=':productId' element={<Product />} />
+        </Route>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LoginSignup />} />
+      </Routes>
+      </div> 
   );
 }
 
--- src/components/assets/Navbar/Navbar.jsx
+++ src/components/assets/Navbar/Navbar.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ShoppingCartIcon } from '@heroicons/react/outline'; // Import the shopping cart icon from Heroicons
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
   return (
     <div className='navbar'>
       <div className='navbar-logo'>
         <p>300 ML</p>
       </div>
       <ul className='nav-menu'>
-        <li>Home<hr /></li>
-        <li>Wine</li>
-        <li>Gin</li>
-        <li>Whiskey</li>
-        <li>Vodka</li>
-        <li>Beer</li>
+        <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration: 'None'}} to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("wine")}}><Link style={{textDecoration: 'None'}} to='/wine'>Wine</Link>{menu==="wine"?<hr/>:<></>}</li>
+        <li onClick={()=>setMenu("gin")}><Link style={{textDecoration: 'None'}} to='/gin'>Gin</Link>{menu==="gin"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("whiskey")}}><Link style={{textDecoration: 'None'}} to='/whiskey'>Whiskey</Link>{menu==="whiskey"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("vodka")}}><Link style={{textDecoration: 'None'}} to='/vodka'>Vodka</Link>{menu==="vodka"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("beer")}}><Link style={{textDecoration: 'None'}} to='/beer'>Beer</Link>{menu==="beer"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("product")}}><Link style={{textDecoration: 'None'}} to='/product'>Shop</Link>{menu==="product"?<hr/>:<></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <button>Login</button>
+        <Link style={{textDecoration: 'None'}} to='/login'><button>Login</button></Link>
             <div className='cart-icon'>
-                <ShoppingCartIcon className='shopping-cart-icon' />
+                <Link style={{textDecoration: 'None'}} to='/cart'><ShoppingCartIcon className='shopping-cart-icon' /></Link>
             </div>
            
       </div>
