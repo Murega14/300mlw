8d01d76b52de2a531a9d286f64a6a6df87f34bd4
Murega14 <tedmurega@gmail.com>
2023-12-12 13:19:54 +0300

    designed the navbar

--- src/components/assets/Navbar/Navbar.jsx
+++ src/components/assets/Navbar/Navbar.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import { ShoppingCartIcon } from '@heroicons/react/outline'; // Import the shopping cart icon from Heroicons
+import './Navbar.css';
 
 const Navbar = () => {
-
-    return (
-        <div className='navbar'>
-             <div className='navbar-logo'>
-                <p>300ML</p>
+  return (
+    <div className='navbar'>
+      <div className='navbar-logo'>
+        <p>300 ML</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Home<hr /></li>
+        <li>Wine</li>
+        <li>Gin</li>
+        <li>Whiskey</li>
+        <li>Vodka</li>
+        <li>Beer</li>
+      </ul>
+      <div className='nav-login-cart'>
+        <button>Login</button>
+            <div className='cart-icon'>
+                <ShoppingCartIcon className='shopping-cart-icon' />
             </div>
-            <ul className='nav-menu'>
-                <li>Home<hr/></li>
-                <li>Shop</li>
-                <li>Contact</li>
-                </ul>
-            <div className='nav-login-cart'>
-                <button>Login</button>
-                <div className='nav-cart-count'>0</div>
-            </div> 
-        </div>
-        )
-    }
+           
+      </div>
+    </div>
+  );
+};
 
 export default Navbar
