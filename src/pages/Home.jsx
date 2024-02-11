ae8325564ac00a5e1ae8e1b9c9510aa59768d40b
Murega14 <tedmurega@gmail.com>
2024-02-12 00:00:13 +0300

    changed navbar and footer  background for light mode users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -237,7 +237,7 @@ const Home = ({
         </CartProvider>
       </div>
 
-      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-0 ">
+      <footer className="bg-gray-400 rounded-lg shadow dark:bg-gray-900 m-0 ">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
