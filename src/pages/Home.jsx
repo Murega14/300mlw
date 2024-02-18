c539b4ac65d79cca6bc59a12d3152d1cfa232d71
Bravin Murega <tedmurega@gmail.com>
2024-02-18 13:59:33 +0000

    changed the footer background and text color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -237,14 +237,14 @@ const Home = ({
         </CartProvider>
       </div>
 
-      <footer className="bg-white rounded-lg shadow bg-[#B3A398] m-0 ">
+      <footer className="rounded-lg shadow bg-[#B3A398] m-0 ">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
               href="https://300mlw.vercel.app"
               className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">
                 300ML
               </span>
             </a>
