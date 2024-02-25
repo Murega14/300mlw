df5919ca43504f6c858ee9354c55db62e17c4dbf
Murega14 <tedmurega@gmail.com>
2024-02-25 23:12:09 +0300

    refractoring

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { CartContext } from '../context/cart.jsx';
 
 const Checkout = () => {
-  const [location, setLocation] = useState('');
-  const [paymentMethod, setPaymentMethod] = useState('');
   const { getCartTotal } = React.useContext(CartContext);
-
-  const handleLocationChange = (e) => {
-    setLocation(e.target.value);
-  };
-
-  const handlePaymentMethodChange = (e) => {
-    setPaymentMethod(e.target.value);
-  };
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can handle the submission logic here, such as sending data to the server
-    console.log('Location:', location);
-    console.log('Payment Method:', paymentMethod);
-  };
 
   return (
     <div>
@@ -28,7 +10,9 @@ const Checkout = () => {
       <h2 className="text-black">Checkout</h2>
       <p className="text-yellow-600"> Ksh{getCartTotal()}</p>
       </header>
-      
+      <section className="">
+
+      </section>
     </div>
   );
 };
